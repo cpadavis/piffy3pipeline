a30f581718b2565a20bb6056e130aabe5b789d3c
Ares Hernandez <aresh@stanford.edu>
2018-08-09 21:03:18 -0700

    Various bug fixes plus a new summary plot (angular moment residuals).

---
+++ angular_moment_residual_plot_maker.py
@@ -0,0 +1,171 @@
+from __future__ import print_function, division
+
+# fix for DISPLAY variable issue
+import matplotlib
+matplotlib.use('Agg')
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+import copy
+
+import os
+import fitsio
+import galsim
+import piff
+import sys
+
+from piff.util import hsm_error, hsm_higher_order, measure_snr
+
+def make_pngs(directory, label, information):
+
+    fig, axss = plt.subplots(nrows=3, ncols=3, figsize=(4 * 4, 4 * 4), squeeze=False)
+    # left column gets the Y coordinate label
+    for axs in axss:
+	for ax in axs:
+	    ax.set(xlabel='angle', ylabel='average moment')
+
+    moments = ["e0", "e1", "e2", "zeta1", "zeta2", "delta1", "delta2"]
+    for i in range(0,3):
+	for j in range(0,3):
+	    if i==2 and j>0:
+		pass
+	    else:
+		index = i*3 + j
+        	axss[i][j].scatter(information[0], information[index+1], label="data")
+        	axss[i][j].scatter(information[0], information[index+8], label="model")
+        	axss[i][j].scatter(information[0], information[index+15], label="difference")
+        	axss[i][j].set_title(moments[index])
+		axss[i][j].legend()
+    plt.tight_layout()
+    fig.savefig("{0}/{1}_{2}_across_angles.png".format(directory, label, psf_type))
+
+def make_plots():
+
+    directory = "{0}/00{1}".format(core_directory, exposure)
+    graph_values_directory = "{0}/graph_values_npy_storage".format(core_directory)
+
+
+
+    for label in ["test", "train"]:
+        # for example, you could have psf_type="optatmo_const_gpvonkarman_meanified"
+        filename = "{0}/shapes_{1}_psf_{2}.h5".format(directory, label, psf_type)
+
+        shapes = pd.read_hdf(filename)
+
+        u = np.array(shapes['u'].tolist())
+        v = np.array(shapes['v'].tolist())
+        raw_angles = np.degrees(np.arctan2(v,u))
+        angles = []
+        for raw_angle in raw_angles:
+            if raw_angle < 0:
+                angle = raw_angle + 360.0
+            else:
+                angle = raw_angle
+            angles.append(angle)
+        angles = np.array(angles)
+
+        data_e0 = np.array(shapes['data_e0'].tolist())
+        model_e0 = np.array(shapes['model_e0'].tolist())
+        difference_e0 = np.array(shapes['de0'].tolist())
+
+        data_e1 = np.array(shapes['data_e1'].tolist())
+        model_e1 = np.array(shapes['model_e1'].tolist())
+        difference_e1 = np.array(shapes['de1'].tolist())
+
+        data_e2 = np.array(shapes['data_e2'].tolist())
+        model_e2 = np.array(shapes['model_e2'].tolist())
+        difference_e2 = np.array(shapes['de2'].tolist())
+
+
+        data_zeta1 = np.array(shapes['data_zeta1'].tolist())
+        model_zeta1 = np.array(shapes['model_zeta1'].tolist())
+        difference_zeta1 = np.array(shapes['dzeta1'].tolist())
+
+        data_zeta2 = np.array(shapes['data_zeta2'].tolist())
+        model_zeta2 = np.array(shapes['model_zeta2'].tolist())
+        difference_zeta2 = np.array(shapes['dzeta2'].tolist())
+
+        data_delta1 = np.array(shapes['data_delta1'].tolist())
+        model_delta1 = np.array(shapes['model_delta1'].tolist())
+        difference_delta1 = np.array(shapes['ddelta1'].tolist())
+
+        data_delta2 = np.array(shapes['data_delta2'].tolist())
+        model_delta2 = np.array(shapes['model_delta2'].tolist())
+        difference_delta2 = np.array(shapes['ddelta2'].tolist())
+
+        information = np.empty([22,36])
+        information[0] = np.array([5.0,15.0,25.0,35.0,45.0,55.0,65.0,75.0,85.0,95.0,105.0,115.0,125.0,135.0,145.0,155.0,165.0,175.0,185.0,195.0,205.0,215.0,225.0,235.0,245.0,255.0,265.0,275.0,285.0,295.0,305.0,315.0,325.0,335.0,345.0,355.0])
+
+        data_final_e0, bin_edges, binnumber = stats.binned_statistic(x=angles,values=data_e0, statistic="mean",bins=36, range=(0.0,360.0))
+        data_final_e1, bin_edges, binnumber = stats.binned_statistic(x=angles,values=data_e1, statistic="mean",bins=36, range=(0.0,360.0))
+        data_final_e2, bin_edges, binnumber = stats.binned_statistic(x=angles,values=data_e2, statistic="mean",bins=36, range=(0.0,360.0))
+        data_final_zeta1, bin_edges, binnumber = stats.binned_statistic(x=angles,values=data_zeta1, statistic="mean",bins=36, range=(0.0,360.0))
+        data_final_zeta2, bin_edges, binnumber = stats.binned_statistic(x=angles,values=data_zeta2, statistic="mean",bins=36, range=(0.0,360.0))
+        data_final_delta1, bin_edges, binnumber = stats.binned_statistic(x=angles,values=data_delta1, statistic="mean",bins=36, range=(0.0,360.0))
+        data_final_delta2, bin_edges, binnumber = stats.binned_statistic(x=angles,values=data_delta2, statistic="mean",bins=36, range=(0.0,360.0))
+
+        model_final_e0, bin_edges, binnumber = stats.binned_statistic(x=angles,values=model_e0, statistic="mean",bins=36, range=(0.0,360.0))
+        model_final_e1, bin_edges, binnumber = stats.binned_statistic(x=angles,values=model_e1, statistic="mean",bins=36, range=(0.0,360.0))
+        model_final_e2, bin_edges, binnumber = stats.binned_statistic(x=angles,values=model_e2, statistic="mean",bins=36, range=(0.0,360.0))
+        model_final_zeta1, bin_edges, binnumber = stats.binned_statistic(x=angles,values=model_zeta1, statistic="mean",bins=36, range=(0.0,360.0))
+        model_final_zeta2, bin_edges, binnumber = stats.binned_statistic(x=angles,values=model_zeta2, statistic="mean",bins=36, range=(0.0,360.0))
+        model_final_delta1, bin_edges, binnumber = stats.binned_statistic(x=angles,values=model_delta1, statistic="mean",bins=36, range=(0.0,360.0))
+        model_final_delta2, bin_edges, binnumber = stats.binned_statistic(x=angles,values=model_delta2, statistic="mean",bins=36, range=(0.0,360.0))
+
+        difference_final_e0, bin_edges, binnumber = stats.binned_statistic(x=angles,values=difference_e0, statistic="mean",bins=36, range=(0.0,360.0))
+        difference_final_e1, bin_edges, binnumber = stats.binned_statistic(x=angles,values=difference_e1, statistic="mean",bins=36, range=(0.0,360.0))
+        difference_final_e2, bin_edges, binnumber = stats.binned_statistic(x=angles,values=difference_e2, statistic="mean",bins=36, range=(0.0,360.0))
+        difference_final_zeta1, bin_edges, binnumber = stats.binned_statistic(x=angles,values=difference_zeta1, statistic="mean",bins=36, range=(0.0,360.0))
+        difference_final_zeta2, bin_edges, binnumber = stats.binned_statistic(x=angles,values=difference_zeta2, statistic="mean",bins=36, range=(0.0,360.0))
+        difference_final_delta1, bin_edges, binnumber = stats.binned_statistic(x=angles,values=difference_delta1, statistic="mean",bins=36, range=(0.0,360.0))
+        difference_final_delta2, bin_edges, binnumber = stats.binned_statistic(x=angles,values=difference_delta2, statistic="mean",bins=36, range=(0.0,360.0))
+
+
+        information[1] = data_final_e0
+        information[2] = data_final_e1
+        information[3] = data_final_e2
+        information[4] = data_final_zeta1
+        information[5] = data_final_zeta2
+        information[6] = data_final_delta1
+        information[7] = data_final_delta2
+
+        information[8] = model_final_e0
+        information[9] = model_final_e1
+        information[10] = model_final_e2
+        information[11] = model_final_zeta1
+        information[12] = model_final_zeta2
+        information[13] = model_final_delta1
+        information[14] = model_final_delta2
+
+        information[15] = difference_final_e0
+        information[16] = difference_final_e1
+        information[17] = difference_final_e2
+        information[18] = difference_final_zeta1
+        information[19] = difference_final_zeta2
+        information[20] = difference_final_delta1
+        information[21] = difference_final_delta2
+
+        np.save("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure),information)
+
+        make_pngs(directory=directory, label=label, information=information)
+  
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exposure')
+    parser.add_argument('--core_directory')
+    parser.add_argument('--psf_type')
+    options = parser.parse_args()
+    exposure = options.exposure
+    core_directory = options.core_directory
+    psf_type = options.psf_type
+    kwargs = vars(options)
+    del kwargs['exposure']
+    del kwargs['core_directory']
+    del kwargs['psf_type']
+    make_plots()
---
+++ call_angular_moment_residual_plot_maker_part2.py
@@ -0,0 +1,242 @@
+from __future__ import print_function, division
+
+# fix for DISPLAY variable issue
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+from scipy import stats
+from astropy.io import fits
+import fitsio
+import matplotlib.pyplot as plt
+import os
+import glob
+
+import lmfit
+import galsim
+import piff
+
+#from zernike import Zernike
+import copy
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+
+
+
+def make_call():
+    core_directory = os.path.realpath(__file__)
+    program_name = core_directory.split("/")[-1]
+    core_directory = core_directory.split("/{0}".format(program_name))[0]
+    graph_values_directory = "{0}/graph_values_npy_storage".format(core_directory)
+    graph_directory = "{0}/multi_exposure_graphs/{1}_angular_moment_residual_plots_averaged_across_exposures".format(core_directory, psf_type)
+    terminal_command = os.system("mkdir {0}".format(graph_directory))
+    source_directory = np.load("{0}/source_directory_name.npy".format(core_directory))[0]
+    original_exposures = glob.glob("{0}/*".format(source_directory))
+    original_exposures = [original_exposure.split("/")[-1] for original_exposure in original_exposures]
+    if band=="all":
+	exposures = original_exposures
+    else:
+	exposures = []
+	for original_exposure in original_exposures:
+	    skip=False
+	    for index in range(1,63):
+		try:
+		    band_test_file = "{0}/{1}/psf_cat_{1}_{2}.fits".format(source_directory, original_exposure, index)
+		    hdu = fits.open(band_test_file)
+		    break
+		except:
+		    if index==62:
+			skip = True
+		    else:
+		        pass
+	    if skip==True:
+		continue
+	    try:
+		band_test_file = "{0}/{1}/exp_psf_cat_{1}.fits".format(source_directory, original_exposure)
+		hdu_c = fits.open(band_test_file)
+		filter_name = hdu_c[1].data['band'][0][0]
+		print(filter_name)
+	    except:
+		try:
+    	    	    filter_name = hdu[3].data['band'][0]
+		except:
+		    continue
+    	    if filter_name==band:
+                exposures.append(original_exposure)  
+	graph_directory = graph_directory + "/angular_moment_residual_plots_just_for_filter_{0}".format(band)
+	os.system("mkdir {0}".format(graph_directory))
+
+    for label in ["test", "train"]:
+
+        data_final_e0 = []
+        model_final_e0 = []
+        difference_final_e0 = []
+
+        data_final_e1 = []
+        model_final_e1 = []
+        difference_final_e1 = []
+
+        data_final_e2 = []
+        model_final_e2 = []
+        difference_final_e2 = []
+
+        data_final_zeta1 = []
+        model_final_zeta1 = []
+        difference_final_zeta1 = []
+
+        data_final_zeta2 = []
+        model_final_zeta2 = []
+        difference_final_zeta2 = []
+
+        data_final_delta1 = []
+        model_final_delta1 = []
+        difference_final_delta1 = []
+
+        data_final_delta2 = []
+        model_final_delta2 = []
+        difference_final_delta2 = []
+
+        for exposure_i, exposure in enumerate(exposures):
+            try:
+                # for example, you could have psf_type="optatmo_const_gpvonkarman_meanified"
+                data_final_e0.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[1])
+                model_final_e0.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[8])        
+                difference_final_e0.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[15])                
+
+                data_final_e1.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[2])
+                model_final_e1.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[9])        
+                difference_final_e1.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[16])              
+
+                data_final_e2.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[3])
+                model_final_e2.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[10])        
+                difference_final_e2.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[17])  
+
+
+
+                data_final_zeta1.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[4])
+                model_final_zeta1.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[11])        
+                difference_final_zeta1.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[18])                
+
+                data_final_zeta2.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[5])
+                model_final_zeta2.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[12])        
+                difference_final_zeta2.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[19])              
+
+                data_final_delta1.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[6])
+                model_final_delta1.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[13])        
+                difference_final_delta1.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[20])        
+
+                data_final_delta2.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[7])
+                model_final_delta2.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[14])        
+                difference_final_delta2.append(np.load("{0}/{1}_{2}_angular_information_{3}.npy".format(graph_values_directory, label, psf_type, exposure))[21])        
+            except:
+                pass
+
+        final_angles = np.array([5.0,15.0,25.0,35.0,45.0,55.0,65.0,75.0,85.0,95.0,105.0,115.0,125.0,135.0,145.0,155.0,165.0,175.0,185.0,195.0,205.0,215.0,225.0,235.0,245.0,255.0,265.0,275.0,285.0,295.0,305.0,315.0,325.0,335.0,345.0,355.0])
+        print(data_final_e0) 
+        data_final_e0 = np.nanmean(np.array(data_final_e0),axis=0)
+        model_final_e0 = np.nanmean(np.array(model_final_e0),axis=0)
+        difference_final_e0 = np.nanmean(np.array(difference_final_e0),axis=0)
+
+        data_final_e1 = np.nanmean(np.array(data_final_e1),axis=0)
+        model_final_e1 = np.nanmean(np.array(model_final_e1),axis=0)
+        difference_final_e1 = np.nanmean(np.array(difference_final_e1),axis=0)
+
+        data_final_e2 = np.nanmean(np.array(data_final_e2),axis=0)
+        model_final_e2 = np.nanmean(np.array(model_final_e2),axis=0)
+        difference_final_e2 = np.nanmean(np.array(difference_final_e2),axis=0)
+
+        data_final_zeta1 = np.nanmean(np.array(data_final_zeta1),axis=0)
+        model_final_zeta1 = np.nanmean(np.array(model_final_zeta1),axis=0)
+        difference_final_zeta1 = np.nanmean(np.array(difference_final_zeta1),axis=0)
+
+        data_final_zeta2 = np.nanmean(np.array(data_final_zeta2),axis=0)
+        model_final_zeta2 = np.nanmean(np.array(model_final_zeta2),axis=0)
+        difference_final_zeta2 = np.nanmean(np.array(difference_final_zeta2),axis=0)
+
+        data_final_delta1 = np.nanmean(np.array(data_final_delta1),axis=0)
+        model_final_delta1 = np.nanmean(np.array(model_final_delta1),axis=0)
+        difference_final_delta1 = np.nanmean(np.array(difference_final_delta1),axis=0)
+
+        data_final_delta2 = np.nanmean(np.array(data_final_delta2),axis=0)
+        model_final_delta2 = np.nanmean(np.array(model_final_delta2),axis=0)
+        difference_final_delta2 = np.nanmean(np.array(difference_final_delta2),axis=0)    
+
+        print(final_angles)
+        print(data_final_e0)    
+
+
+        plt.figure()
+        plt.scatter(final_angles,data_final_e0, label="data")
+        plt.scatter(final_angles,model_final_e0, label="model")
+        plt.scatter(final_angles,difference_final_e0, label="difference")
+        plt.title("e0 across angles")
+        plt.legend()
+        plt.savefig("{0}/{1}_e0_across_angles.png".format(graph_directory, label))
+
+        plt.figure()
+        plt.scatter(final_angles,data_final_e1, label="data")
+        plt.scatter(final_angles,model_final_e1, label="model")
+        plt.scatter(final_angles,difference_final_e1, label="difference")
+        plt.title("e1 across angles")
+        plt.legend()
+        plt.savefig("{0}/{1}_e1_across_angles.png".format(graph_directory, label))
+
+        plt.figure()
+        plt.scatter(final_angles,data_final_e2, label="data")
+        plt.scatter(final_angles,model_final_e2, label="model")
+        plt.scatter(final_angles,difference_final_e2, label="difference")
+        plt.title("e2 across angles")
+        plt.legend()
+        plt.savefig("{0}/{1}_e2_across_angles.png".format(graph_directory, label))    
+
+
+        plt.figure()
+        plt.scatter(final_angles,data_final_zeta1, label="data")
+        plt.scatter(final_angles,model_final_zeta1, label="model")
+        plt.scatter(final_angles,difference_final_zeta1, label="difference")
+        plt.title("zeta1 across angles")
+        plt.legend()
+        plt.savefig("{0}/{1}_zeta1_across_angles.png".format(graph_directory, label))
+
+        plt.figure()
+        plt.scatter(final_angles,data_final_zeta2, label="data")
+        plt.scatter(final_angles,model_final_zeta2, label="model")
+        plt.scatter(final_angles,difference_final_zeta2, label="difference")
+        plt.title("zeta2 across angles")
+        plt.legend()
+        plt.savefig("{0}/{1}_zeta2_across_angles.png".format(graph_directory, label))
+
+        plt.figure()
+        plt.scatter(final_angles,data_final_delta1, label="data")
+        plt.scatter(final_angles,model_final_delta1, label="model")
+        plt.scatter(final_angles,difference_final_delta1, label="difference")
+        plt.title("delta1 across angles")
+        plt.legend()
+        plt.savefig("{0}/{1}_delta1_across_angles.png".format(graph_directory, label))
+
+        plt.figure()
+        plt.scatter(final_angles,data_final_delta2, label="data")
+        plt.scatter(final_angles,model_final_delta2, label="model")
+        plt.scatter(final_angles,difference_final_delta2, label="difference")
+        plt.title("delta2 across angles")
+        plt.legend()
+        plt.savefig("{0}/{1}_delta2_across_angles.png".format(graph_directory, label))  
+
+
+
+
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--psf_type')
+    parser.add_argument('--band')
+    options = parser.parse_args()
+    psf_type = options.psf_type
+    band = options.band
+    kwargs = vars(options)
+    del kwargs['psf_type']
+    del kwargs['band']
+    make_call()
